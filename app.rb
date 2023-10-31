c2a6697f35f08e8260af25c0e038c3f22b87e234
stanley-world <146215090+stanley-world@users.noreply.github.com>
2023-10-31 03:59:36 +0000

    giraffe

--- app.rb
+++ app.rb
@@ -9,7 +9,49 @@ get("/") do
 end
 
 get("/rock") do 
-  "
-  <h1>rock</h1>
-  "
+  moves = ["rock", "paper", "scissors"]
+  
+  @comp_move = moves.sample
+
+  if @comp_move == 'rock'
+    @outcome = 'tied'
+  elsif @comp_move == 'paper'
+    @outcome = 'lost'
+  else 
+    @outcome = 'won'
+  end 
+
+  erb(:zebra)
+end 
+
+get("/paper") do 
+  moves = ["rock", "paper", "scissors"]
+  
+  @comp_move = moves.sample
+
+  if @comp_move == 'paper'
+    @outcome = 'tied'
+  elsif @comp_move == 'scissors'
+    @outcome = 'lost'
+  else 
+    @outcome = 'won'
+  end 
+
+  erb(:giraffe)
+end 
+
+get("/scissors") do 
+  moves = ["rock", "paper", "scissors"]
+  
+  @comp_move = moves.sample
+
+  if @comp_move == 'scissors'
+    @outcome = 'tied'
+  elsif @comp_move == 'rock'
+    @outcome = 'lost'
+  else 
+    @outcome = 'won'
+  end 
+
+  erb(:zebra)
 end 
